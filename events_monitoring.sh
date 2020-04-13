83afa6b2374dffbef591f1d15383bf49d8e19c97
fjodoin <filipjodoin@hotmail.com>
2020-04-13 14:02:36 -0400

    script message changed when launched

--- events_monitoring.sh
+++ events_monitoring.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo [SMARTFLOW] openhab2/events.log monitoring scripted launched [...]
+echo [SMARTFLOW] [PATH_to_dir]/openhab2/events.log monitoring.sh scripted launched [...]
 tail -n0 -f /var/log/openhab2/events.log | python3 /home/corelee/Desktop/SmartFlow/events_parser.py
